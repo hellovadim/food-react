740903f8be2069f8b5b1fe2394d389b948893025
Vadim <hellovados@gmail.com>
2021-05-16 00:44:07 +0300

    Начал создание нового проекта на Udemy

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Header() {
+  return (
+    <nav className="teal grey darken-3">
+      <div className="nav-wrapper">
+        <span className="brand-logo">Shop</span>
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+          <li>
+            <a href="sass.html">Repo</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+export { Header };
