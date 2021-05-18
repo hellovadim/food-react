6b0966ad745e05ae4c2ef1563d513e603f0adb47
Vadim <hellovados@gmail.com>
2021-05-18 03:01:03 +0300

    добавил строку поиска с гет парметром

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,47 @@
-import {useState, useEffect} from 'react';
-import {Preloader} from '../components/Preloader';
-import {getAllCategories} from '../api'
-import {CategoryList} from '../components/CategoryList';
+import { useState, useEffect } from "react";
+import { useLocation, useHistory } from "react-router-dom";
+import { Preloader } from "../components/Preloader";
+import { getAllCategories } from "../api";
+import { CategoryList } from "../components/CategoryList";
+import { Search } from "../components/Search";
 
+function Home() {
+  const [catalog, setCatalog] = useState([]);
+  const [filteredCatalog, setFilteredCatalog] = useState([]);
+  const { pathname, search } = useLocation();
+  const { push } = useHistory();
+  const handleSearch = (str) => {
+    setFilteredCatalog(
+      catalog.filter((item) =>
+        item.strCategory.toLowerCase().includes(str.toLowerCase())
+      )
+    );
+    push({
+      pathname,
+      search: `?search=${str}`,
+    });
+  };
 
-function Home(){
-    const [catalog, setCatalog] = useState([]);
-    useEffect(() => {
-        getAllCategories().then(data => {
-            setCatalog(data.categories)
-        })}, [])
-
-    return <>
-        {
-            !catalog.length ? <Preloader/> : (
-                <CategoryList catalog={catalog}/>
+  useEffect(() => {
+    getAllCategories().then((data) => {
+      setCatalog(data.categories);
+      setFilteredCatalog(
+        search
+          ? data.categories.filter((item) =>
+              item.strCategory
+                .toLowerCase()
+                .includes(search.split("=")[1].toLowerCase())
             )
-        }
-        </>
+          : data.categories
+      );
+    });
+  }, [search]);
+
+  return (
+    <>
+      <Search cb={handleSearch} />
+      {!catalog.length ? <Preloader /> : <CategoryList catalog={filteredCatalog} />}
+    </>
+  );
 }
-export {Home}
+export { Home };
